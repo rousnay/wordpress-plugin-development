553855853373dace1f93c99a510e13dfb07a80c6
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-01-21 17:34:37 +0600

    Basic structure with admin sub-menu

---
+++ basic-plugin/basic-plugin.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name:  Basic Plugin
+Description:  A WordPress Plugin with core features
+Plugin URI:   https://wittyplex.com/wordpress
+Author:       Mozahidur Rahman
+Version:      1.0
+Text Domain:  myplugin
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
+// exit if file is called directly
+if (!defined('ABSPATH')) {
+
+    exit;
+}
+
+
+// if admin area
+if ( is_admin() ) {
+
+    // include dependencies
+    require_once plugin_dir_path( __FILE__ ) . 'admin/admin-menu.php';
+    require_once plugin_dir_path( __FILE__ ) . 'admin/settings-page.php';
+
+}
+
